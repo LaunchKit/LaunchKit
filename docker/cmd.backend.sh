0ea6f0b6ed30772149e9e5160d9d9e00c64522c2
Viktor Kifer <viktor.kifer.dev@gmail.com>
2018-06-14 08:41:13 +0300

    Fix docker support

--- docker/cmd.backend.sh
+++ docker/cmd.backend.sh
@@ -9,7 +9,19 @@ sed -i "s|'USER': 'vagrant'|'USER': '${POSTGRES_USER}'|" backend/settings.py
 sed -i "s|'PASSWORD': ''|'PASSWORD': '${POSTGRES_PASSWORD}'|" backend/settings.py
 sed -i "s|'HOST': 'localhost'|'HOST': '${POSTGRES_HOST}'|" backend/settings.py
 
+# sed -i "s|APP_ENGINE_PHOTOS_UPLOAD_BASE_PATH = \"http://localhost:9102\"|APP_ENGINE_PHOTOS_UPLOAD_BASE_PATH = \"http://gae:9102\"|" backend/settings.py
+
 # Fix Redis settings
 sed -i "s|redis://localhost:6379/0|${REDIS_URL}|" backend/settings.py
 
+go run devproxy.go 0.0.0.0:9102 gae:9103 &
+
+echo "Migrating database"
+python manage.py migrate
+
+celery worker -A backend.celery_app -Q \
+  	celery,email,ingestion,archive,gae,slack,itunes,itunesux,itunesfetch,appstore,sessions \
+  	-lINFO -B -Ofair --concurrency=1 &
+
+echo "Starting server"
 python manage.py runserver 0.0.0.0:${PORT}
--- docker/cmd.gae.sh
+++ docker/cmd.gae.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/local/google_appengine/dev_appserver.py --log_level debug --skip_sdk_update_check --port ${PORT} --admin_port ${ADMIN_PORT} --storage_path ./.dev_gae_storage .
+dev_appserver.py --log_level debug --host 0.0.0.0 --skip_sdk_update_check --port 9103 --admin_port 9104 --storage_path ./.dev_gae_storage gae &
+go run devproxy.go 0.0.0.0:9102 localhost:9103
